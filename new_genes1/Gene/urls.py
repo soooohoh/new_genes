0dc6681dfb9361fd03f70cb9bfa14f323cb686a1
parkchihoon <qkrqod4010@naver.com>
2023-05-05 18:29:26 +0900

    네번째 커밋 avatar게임 템플릿 생성, urlpatterns 수정

--- new_genes1/Gene/urls.py
+++ new_genes1/Gene/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('mypage/', views.my_page, name='my_page'),
     path('daily_question/', views.daily_question, name='daily_question'),
-    path('gene_register<int:property_id>/', views.gene_register, name='gene_register')
+    path('gene_register<int:property_id>/', views.gene_register, name='gene_register'),
+    path('avartar_game/', views.avatar_game, name='avatar_game')
 ]
