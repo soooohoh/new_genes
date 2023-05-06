886dad7695fe721a07f21a88e731a717c9423be4
parkchihoon <qkrqod4010@naver.com>
2023-05-06 22:52:32 +0900

    생활습관, 식습관 출력 url추가

--- new_genes1/Gene/urls.py
+++ new_genes1/Gene/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('mypage/', views.my_page, name='my_page'),
     path('daily_question/', views.daily_question, name='daily_question'),
     path('gene_register<int:property_id>/', views.gene_register, name='gene_register'),
+    path('gene_register/<str:gene_name>/', views.habits, name='habits'),
     path('avartar_game/', views.avatar_game, name='avatar_game')
 ]
