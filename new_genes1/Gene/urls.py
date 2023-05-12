1ac226821d27edce088c335e8b2bc83a5e89ecfe
parkchihoon <qkrqod4010@naver.com>
2023-05-12 23:52:44 +0900

    models.py수정 생활 꿀팁 링크 추가

--- new_genes1/Gene/urls.py
+++ new_genes1/Gene/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('mypage/', views.my_page, name='my_page'),
+    path('mypage/properties/', views.property_confirm, name='property_confirm'),
+    path('mypage/properties/gene_register/<int:property_id>', views.gene_register, name='gene_register'),
+    path('mypage/properties/gene_register/<str:gene_name>', views.habits, name='habits'),
     path('daily_question/', views.daily_question, name='daily_question'),
-    path('gene_register<int:property_id>/', views.gene_register, name='gene_register'),
-    path('gene_register/<str:gene_name>/', views.habits, name='habits'),
+    #path('gene_register<int:property_id>/', views.gene_register, name='gene_register'),
     path('avartar_game/', views.avatar_game, name='avatar_game')
 ]
--- new_genes1/Gene/views.py
+++ new_genes1/Gene/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Property, Gene, Eating_Habits
+from Gene.models import User
 from .forms import GeneForm
-from .models import Lifestyle
+from Gene.models import LifeStyle
 # Create your views here.
 def home(request):
     return render(request, 'Gene/home.html')
@@ -13,24 +14,26 @@ def my_page(request):
 
     return render(request, 'Gene/my_page.html', context=context)
 
-def daily_question(request):
-    return render(request, 'Gene/daily_question.html')
+def property_confirm(request):
+    context=dict()
+    data = Property.objects.all()
+    context['property'] = data
+    
+    return render(request, 'Gene/property_confirm.html', context=context)
 
 def gene_register(request, property_id):
     if request.POST=="POST":
-        pass
+        return render(request, 'Gene/property_confirm.html')
                     
     else:
-        data = Gene.objects.filter(property_id_id=property_id)
+        data = Gene.objects.filter(property=property_id)
         context=dict()
         context['gene']=data
-        form = GeneForm()
-        context['form']=form
         return render(request, 'Gene/gene_register.html', context=context)
 
 def habits(request, gene_name):    
     data = Eating_Habits.objects.filter(gene_name=gene_name) # get방식으로 가져오면 multiple에러???
-    style_data = Lifestyle.objects.filter(gene_name=gene_name)
+    style_data = LifeStyle.objects.filter(gene_name=gene_name)
     context=dict()
     context['data'] = data
     context['style_data'] = style_data
@@ -39,3 +42,7 @@ def habits(request, gene_name):
 
 def avatar_game(request):
     return render(request, 'Gene/avatar_game.html')
+
+
+def daily_question(request):
+    return render(request, 'Gene/daily_question.html')
