860abb24fb9d5d8e9ca5975cc0991dd8464379ee
parkchihoon <qkrqod4010@naver.com>
2023-05-22 20:48:18 +0900

    유전자 검사 결과 등록 기능 구현

--- new_genes1/Gene/models.py
+++ new_genes1/Gene/models.py
@@ -16,192 +16,193 @@ class User(AbstractUser):
         (2, 'TA'),
         (3, 'AA')
     ]
-    FTO_gene = models.IntegerField(choices=rating_FTO, default=None, null=True,)
+    FTO_gene = models.IntegerField(choices=rating_FTO, default=None, null=True)
     rating_MC4R = [
         (1, 'TT'),
         (2, 'TC'),
         (3, 'CC')
     ]
-    MC4R_gene = models.IntegerField(choices=rating_MC4R, default=None, null=True,)
+    MC4R_gene = models.IntegerField(choices=rating_MC4R, default=None, null=True)
     rating_BDNF = [
         (1, 'AA'),
         (2, 'AG'),
         (3, 'GG')
     ]
-    BDNF_gene = models.IntegerField(choices=rating_BDNF, default=None, null=True, )
-    rating_OCA2 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+    BDNF_gene = models.IntegerField(choices=rating_BDNF, default=None, null=True)
+    rating_OCA2=[
+        (1, 'TT'),
+        (2, 'TC'),
+        (3, 'CC')
     ]
-    OCA2_gene = models.IntegerField(choices=rating_OCA2, default=None)
+    OCA2_gene = models.IntegerField(choices=rating_OCA2, null=True, default=None)
     rating_MC1R = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GA'),
+        (3, 'AA')
     ]
-    MC1R_gene = models.IntegerField(choices=rating_MC1R, default=None)
+    MC1R_gene = models.IntegerField(choices=rating_MC1R, null=True, default=None)
     rating_chr20p11_1 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GA'),
+        (3, 'AA')
     ]
-    chr20p11_1 = models.IntegerField(choices=rating_chr20p11_1, default=None)
+    chr20p11_1 = models.IntegerField(choices=rating_chr20p11_1, null=True, default=None)
     rating_chr20p11_2 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'CC'),
+        (2, 'CT'),
+        (3, 'TT')
     ]
-    chr20p11_2 = models.IntegerField(choices=rating_chr20p11_2, default=None)
+    chr20p11_2 = models.IntegerField(choices=rating_chr20p11_2, null=True, default=None)
     rating_IL2RA = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'TT'),
+        (2, 'TC'),
+        (3, 'CC')
     ]
-    IL2RA = models.IntegerField(choices=rating_IL2RA, default=None)
+    IL2RA = models.IntegerField(choices=rating_IL2RA, null=True, default=None)
     rating_HLA_DQB1 = [
         (1, 'AA'),
         (2, 'AG'),
         (3, 'GG')
     ]
-    HLA_DQB1 = models.IntegerField(choices=rating_HLA_DQB1, default=None)
+    HLA_DQB1 = models.IntegerField(choices=rating_HLA_DQB1, null=True, default=None)
     rating_EDAR = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GA'),
+        (3, 'AA')
     ]
-    EDAR = models.IntegerField(choices=rating_EDAR, default=None)
+    EDAR = models.IntegerField(choices=rating_EDAR, null=True, default=None)
     rating_AGER_1 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'CC'),
+        (2, 'CT'),
+        (3, 'TT')
     ]
-    AGER_1 = models.IntegerField(choices=rating_AGER_1, default=None)
+    AGER_1 = models.IntegerField(choices=rating_AGER_1, null=True, default=None)
     rating_AGER_2 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GA'),
+        (3, 'AA')
     ]
-    AGER_2 = models.IntegerField(choices=rating_AGER_2, default=None)
+    AGER_2 = models.IntegerField(choices=rating_AGER_2, null=True, default=None)
     rating_GCKR = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'CC'),
+        (2, 'CT'),
+        (3, 'TT')
     ]
-    GCKR = models.IntegerField(choices=rating_GCKR, default=None)
+    GCKR = models.IntegerField(choices=rating_GCKR, null=True, default=None)
     rating_ANGPTL3 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GT'),
+        (3, 'TT')
     ]
-    ANGPTL3 = models.IntegerField(choices=rating_ANGPTL3, default=None)
+    ANGPTL3 = models.IntegerField(choices=rating_ANGPTL3, null=True, default=None)
     rating_TRIB1 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'TT'),
+        (2, 'TA'),
+        (3, 'AA')
     ]
-    TRIB1 = models.IntegerField(choices=rating_TRIB1, default=None)
+    TRIB1 = models.IntegerField(choices=rating_TRIB1, null=True, default=None)
     rating_HMGCR = [
         (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (2, 'AT'),
+        (3, 'TT')
     ]
-    HMGCR = models.IntegerField(choices=rating_HMGCR, default=None)
+    HMGCR = models.IntegerField(choices=rating_HMGCR, null=True, default=None)
     rating_ABO = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'CC'),
+        (2, 'CA'),
+        (3, 'AA')
     ]
-    ABO = models.IntegerField(choices=rating_ABO, default=None)
-    rating_ABCA1 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
-    ]
-    ABCA1 = models.IntegerField(choices=rating_ABCA1, default=None)
+    ABO = models.IntegerField(choices=rating_ABO, null=True, default=None)
     rating_LIPG = [
-        (1, 'AA'),
-        (2, 'AG'),
+        (1, 'TT'),
+        (2, 'TG'),
         (3, 'GG')
     ]
-    LIPG = models.IntegerField(choices=rating_LIPG, default=None)
+    LIPG = models.IntegerField(choices=rating_LIPG, null=True, default=None)
     rating_DGKB_TMEM195 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GT'),
+        (3, 'TT')
     ]
-    DGKB_TMEM195 = models.IntegerField(choices=rating_DGKB_TMEM195, default=None)
+    DGKB_TMEM195 = models.IntegerField(choices=rating_DGKB_TMEM195, null=True, default=None)
     rating_CDKN2A_2B = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'CC'),
+        (2, 'CT'),
+        (3, 'TT')
     ]
-    CDKN2A_2B = models.IntegerField(choices=rating_CDKN2A_2B, default=None)
+    CDKN2A_2B = models.IntegerField(choices=rating_CDKN2A_2B, null=True, default=None)
     rating_GCK = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GA'),
+        (3, 'AA')
     ]
-    GCK = models.IntegerField(choices=rating_GCK, default=None)
+    GCK = models.IntegerField(choices=rating_GCK, null=True, default=None)
     rating_GLIS3 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GA'),
+        (3, 'AA')
     ]
-    GLIS3 = models.IntegerField(choices=rating_GLIS3, default=None)
+    GLIS3 = models.IntegerField(choices=rating_GLIS3, null=True, default=None)
     rating_GUCY1A3 = [
         (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (2, 'AC'),
+        (3, 'CC')
     ]
-    GUCY1A3 = models.IntegerField(choices=rating_GUCY1A3, default=None)
+    GUCY1A3 = models.IntegerField(choices=rating_GUCY1A3, null=True, default=None)
+    rating_ABCA1 = [
+        (1, 'CC'),
+        (2, 'CT'),
+        (3, 'TT')
+    ]
+    ABCA1 = models.IntegerField(choices=rating_ABCA1, null=True, default=None)
     rating_FGF5 = [
         (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (2, 'AT'),
+        (3, 'TT')
     ]
-    FGF5 = models.IntegerField(choices=rating_FGF5, default=None)
+    FGF5 = models.IntegerField(choices=rating_FGF5, null=True, default=None)
     rating_ATP2B1 = [
         (1, 'AA'),
         (2, 'AG'),
         (3, 'GG')
     ]
-    ATP2B1 = models.IntegerField(choices=rating_ATP2B1, default=None)
+    ATP2B1 = models.IntegerField(choices=rating_ATP2B1, null=True, default=None)
     rating_NPR3 = [
         (1, 'AA'),
         (2, 'AG'),
         (3, 'GG')
     ]
-    NPR3 = models.IntegerField(choices=rating_NPR3, default=None)
+    NPR3 = models.IntegerField(choices=rating_NPR3, null=True, default=None)
     rating_CYP17A1 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'GG'),
+        (2, 'GA'),
+        (3, 'AA')
     ]
-    CYP17A1 = models.IntegerField(choices=rating_CYP17A1, default=None)
+    CYP17A1 = models.IntegerField(choices=rating_CYP17A1, null=True, default=None)
     rating_SLC23A1_1 = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (1, 'CC'),
+        (2, 'CT'),
+        (3, 'TT')
     ]
-    SLC23A1_1 = models.IntegerField(choices=rating_SLC23A1_1, default=None)
+    SLC23A1_1 = models.IntegerField(choices=rating_SLC23A1_1, null=True, default=None)
     rating_SLC23A1_2 = [
         (1, 'AA'),
         (2, 'AG'),
         (3, 'GG')
     ]
-    SLC23A1_2 = models.IntegerField(choices=rating_SLC23A1_2, default=None)
+    SLC23A1_2 = models.IntegerField(choices=rating_SLC23A1_2, null=True, default=None)
     rating_CYP1A1_CYP1A2 = [
         (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
+        (2, 'AC'),
+        (3, 'CC')
     ]
-    CYP1A1_CYP1A2 = models.IntegerField(choices=rating_CYP1A1_CYP1A2, default=None)
+    CYP1A1_CYP1A2 = models.IntegerField(choices=rating_CYP1A1_CYP1A2, null=True, default=None)
     rating_AHR = [
-        (1, 'AA'),
-        (2, 'AG'),
-        (3, 'GG')
-    ]           
+        (1, 'CC'),
+        (2, 'CT'),
+        (3, 'TT')
+    ]
+    AHR = models.IntegerField(choices=rating_AHR, null=True, default=None)    
 
 
     def __str__(self):
