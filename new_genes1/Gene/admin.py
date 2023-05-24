550ba10986c116ec257b42812aa4cabdfab5afe1
parkchihoon <qkrqod4010@naver.com>
2023-05-24 16:19:05 +0900

    최신커밋

--- new_genes1/Gene/admin.py
+++ new_genes1/Gene/admin.py
@@ -4,10 +4,12 @@ from .models import User
 from .models import Eating_Habits
 from Gene.models import Gene
 from Gene.models import LifeStyle
+from Gene.models import Diary
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Eating_Habits)
 admin.site.register(Gene)
 admin.site.register(LifeStyle)
-UserAdmin.fieldsets += ('Custom fields', {'fields' : ('nickname', 'intro', 'goals', 'profile_picture')}),
+admin.site.register(Diary)
+UserAdmin.fieldsets += ('Custom fields', {'fields' : ('nickname', 'intro', 'goals', 'profile_picture', 'FTO_gene', 'MC4R_gene', 'BDNF_gene')}),
--- new_genes1/new_genes/urls.py
+++ new_genes1/new_genes/urls.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """new_genes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -34,4 +35,45 @@ urlpatterns = [
     path('', include('allauth.urls')),
 ]
 
+=======
+"""new_genes URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from Gene.views import CustomPasswordChangeView
+
+urlpatterns = [
+    # admin Url 패턴
+    path('admin/', admin.site.urls),
+
+    #Gene applicatiion Url 패턴
+    path('', include('Gene.urls')),
+
+
+    
+    # password change Url패턴
+    path("password/change/", CustomPasswordChangeView.as_view(), name="account_change_password"),
+
+    path('', include('allauth.urls')),
+]
+                        #MEDIA에대한 요청이 들어오면...       MEDIA_ROOT안에 있는 media파일을 돌려주어라...
+<<<<<<< HEAD
+=======
+>>>>>>> 4ca6192e24e793c4a9b51441f48dee89750917f2
+>>>>>>> f9377ee7466023420c339835d1f9c9a8974ed04f
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
